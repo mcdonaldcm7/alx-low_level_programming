3332df385d123949eef8458684b210657bab3bfe
LQ McDonald <mcdonald.cm7@gmail.com>
2022-12-08 12:44:29 -0800

    Corrected 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: Prints exactly "Programming is like building a multilingual puzzle
+ *Return: Returns 0
+ */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

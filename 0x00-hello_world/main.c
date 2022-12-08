0dd3e719a3d5645c71c05f3369ee7c9d4adedbeb
LQ McDonald <mcdonald.cm7@gmail.com>
2022-12-08 12:52:39 -0800

    Added 5-printf.c

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints the requested string
+ * Returns: 0
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

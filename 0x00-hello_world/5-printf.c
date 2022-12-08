471deaebb1323ad8e99a1a9b469807b23b7cf5e6
LQ McDonald <mcdonald.cm7@gmail.com>
2022-12-08 12:56:33 -0800

    Corrected 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,10 @@
  *main - Entry point
  *
  *Description: Prints the requested string
- *Returns: 0
+ *Return: 0
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

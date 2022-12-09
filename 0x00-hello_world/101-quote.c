c8472003e181f7a522402d9f462cfe04a9fc0e32
LQ McDonald <mcdonald.cm7@gmail.com>
2022-12-08 20:29:57 -0800

    Corrected 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the requested text.
+ * Return: 1
+ */
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}

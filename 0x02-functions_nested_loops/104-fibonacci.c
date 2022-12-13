3bccecbd0d30053a2f4ce92c5020ad562d55ae1f
LQ McDonald <mcdonald.cm7@gmail.com>
2022-12-13 15:23:55 -0800

    Corrected 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,21 +9,37 @@
 int main(void)
 {
 	int i;
-	unsigned long p1, p2, curr;
+	unsigned long p1, p1m, p2, p2m, curr, currm;
 
 	p1 = 1, p2 = 2;
 	printf("1, 2, ");
-	for (i = 0; i < 96; i++)
+	for (i = 0; i < 91; i++)
 	{
 		curr = p1 + p2;
-		if (i == 95)
-		{
-			printf("%lu", curr);
-			continue;
-		}
 		printf("%lu, ", curr);
 		p1 = p2;
 		p2 = curr;
+		p1m = p1 % 1000000;
+		p2m = p2 % 1000000;
+		currm = curr % 1000000;
+	}
+	p1 -= p1m;
+	p2 -= p2m;
+	curr -= currm;
+	for (i = 0; i < 6; i++)
+	{
+		curr = p1 + p2;
+		currm = p1m + p2m;
+		if (i == 5)
+		{
+			printf("%lu", (curr + currm));
+			continue;
+		}
+		printf("%lu, ", (curr + currm));
+		p1 = p2;
+		p2 = curr;
+		p1m = p2m;
+		p2m = currm;
 	}
 	printf("\n");
 	return (0);

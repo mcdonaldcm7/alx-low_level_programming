b3b37d25be00284e21b7712e9b40579f7901bf4d
LQ McDonald <mcdonald.cm7@gmail.com>
2022-12-13 11:06:47 -0800

    Corrected 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,9 +10,8 @@
  */
 void print_times_table(int n)
 {
-	int i, j, spaces;
+	int i, j;
 
-	spaces = (n * n) / 10;
 	if (n < 0 || n > 15)
 		return;
 
@@ -21,18 +20,22 @@ void print_times_table(int n)
 		for (j = 0; j <= n; j++)
 		{
 			int prod = i * j;
+			int next = i * (j + 1);
 
 			if (j == n)
 			{
 				printf("%d", prod);
 				continue;
 			}
-			if (spaces >= 10)
+			if (next > 99)
 			{
-				printf("%d,   ", prod);
-			} else
+				printf("%d, ", prod);
+			} else if (next > 9)
 			{
 				printf("%d,  ", prod);
+			} else
+			{
+				printf("%d,   ", prod);
 			}
 		}
 		printf("\n");

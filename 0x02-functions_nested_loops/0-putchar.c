e7463d51cdf7b31f733e4b9704c5a6d3994221fc
LQ McDonald <mcdonald.cm7@gmail.com>
2022-12-13 02:03:35 -0800

    Added 0-printchar.c and README.md

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+int _putchar(char c);
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints _putchar
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,5 @@
+This READMEfile contains a detailed description of all the program file(s) contained in this directory
+
+  0-putchar.c : This program prints _putchar followed by a new line
+
+  

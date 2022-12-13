aa26fdf015e170511b270371104f9e181a0a3c98
LQ McDonald <mcdonald.cm7@gmail.com>
2022-12-13 07:40:52 -0800

    Resolved a logical error in 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,27 @@
  */
 void print_to_98(int n)
 {
-	for (; n <= 98; n++)
+	if (n <= 98)
 	{
-		if (n == 98)
+		for (; n <= 98; n++)
 		{
-			printf("98");
-			continue;
+			if (n == 98)
+			{
+				printf("98\n");
+				continue;
+			}
+			printf("%d, ", n);
 		}
-		printf("%d, ", n);
+	} else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("98\n");
+				continue;
+			}
+			printf("%d, ", n);
+		}
 	}
-	_putchar('\n');
 }

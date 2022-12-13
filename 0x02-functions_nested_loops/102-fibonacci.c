fc898d73501442eb85481172a37765153dd9981b
LQ McDonald <mcdonald.cm7@gmail.com>
2022-12-13 14:22:09 -0800

    Corrected 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point
+ *
+ * Description: Prints first 50 fibonacci numbers
+ * Return: void
+ */
+int main(void)
 {
 	int i;
 	unsigned long p1, p2, curr;
 
 	p1 = 1, p2 = 2;
 	printf("1, 2, ");
-	for (i = 0; i <= 48; i++)
+	for (i = 0; i < 48; i++)
 	{
 		curr = p1 + p2;
-		if (i == 48)
+		if (i == 47)
 		{
 			printf("%lu", curr);
 			continue;

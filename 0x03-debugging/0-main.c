a7b8d0ddf7b2bad394ff860439f9599f77de733d
LQ McDonald <mcdonald.cm7@gmail.com>
2022-12-14 06:02:26 -0800

    Corrected 0-main.c and main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
-/**
- * main - Entry Point
+/** 
+ * main - tests function that prints if integer is positive or negative
  *
- * Description: Tests the positive_or_negative function
  * Return: 0
  */
-int main(void)
+
+
+int main(void)	
 {
-	positive_or_negative(0);
-	return (0);
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+	
+	return (0);				
 }

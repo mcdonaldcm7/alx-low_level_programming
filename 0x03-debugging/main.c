9fef702f0f395967765c1c2ff8fb5924c665ecdc
LQ McDonald <mcdonald.cm7@gmail.com>
2022-12-14 05:52:19 -0800

    Added main.c and main.h

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Description: Tests the positive_or_negative function
+ * Return: 0
+ */
+int main(void)
+{
+	positive_or_negative(0);
+	return (0);
+}

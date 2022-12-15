aca0e03f15f9205dcd6ff388165030d7087b6af9
LQ McDonald <mcdonald.cm7@gmail.com>
2022-12-15 05:17:04 -0800

    Made code betty style :)

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _isdigit - Checks whether or not c is a digit
+ *
+ * @c: integer input
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
 int _isdigit(int c)
 {
 	if (c >= 48 && c <= 59)

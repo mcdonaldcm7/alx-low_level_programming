9ef6fd0722ba610b881f91c14022684bb1ebe81a
LQ McDonald <mcdonald.cm7@gmail.com>
2022-12-19 13:26:34 -0800

    Corrected 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,29 @@
 #include "main.h"
 
+/**
+ * _strlen - Gets the length of a string
+ *
+ * @s: Input string
+ *
+ * Return: Return length of s
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * print_rev - Prints the reverse of the input string s
+ *
+ * @s: Input string
+ */
 void print_rev(char *s)
 {
 	int length;

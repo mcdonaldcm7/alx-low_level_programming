fdacfb6746bc80b94a81148adc02dd9d2501a888
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-02 15:17:22 -0800

    Corrected 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -25,27 +25,27 @@ int LQ_strlen(char *s)
  *
  * Return: pointer to the bginning of the locatd substring, or NULL otherwise
  */
-char* _strstr(char* haystack, char* needle)
+char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
 	for (i = 0, j = 0; haystack[i] != '\0'; i++)
 	{
-	if (haystack[i] == needle[j])
-	{
-		int count;
-
-		count = 0;
-		while (haystack[i] == needle[j] && (haystack[i] != '\0' && needle[j] != '\0'))
+		if (haystack[i] == needle[j])
 		{
-			count++;
-			i++;
-			j++;
-		}
+			int count;
 
-		if (count == LQ_strlen(needle))
-			return (&haystack[i - count]);
-		else
+			count = 0;
+			while (haystack[i] == needle[j] && 
+					(haystack[i] != '\0' && needle[j] != '\0'))
+			{
+				count++;
+				i++;
+				j++;
+			}
+
+			if (count == LQ_strlen(needle))
+				return (&haystack[i - count]);
 			j = 0;
 		}
 	}

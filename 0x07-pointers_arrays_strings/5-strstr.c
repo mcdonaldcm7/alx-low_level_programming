7e5df97bf008ba02cde01f58d293a29e06f9d5ef
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-02 15:07:37 -0800

    Added 5-strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * LQ_strlen - Retrieves the length of a string
+ *
+ * @s: Input string
+ *
+ * Return: Length of input string
+ */
+int LQ_strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (s[count] != '\0')
+		count++;
+	return (count);
+}
+
+/**
+ * _strstr - Locates a substring
+ *
+ * @haystack: Input string
+ * @needle: Substring
+ *
+ * Return: pointer to the bginning of the locatd substring, or NULL otherwise
+ */
+char* _strstr(char* haystack, char* needle)
+{
+	int i, j, len;
+
+	for (i = 0, j = 0; haystack[i] != '\0'; i++)
+	{
+	if (haystack[i] == needle[j])
+	{
+		int count;
+
+		count = 0;
+		while (haystack[i] == needle[j] && (haystack[i] != '\0' && needle[j] != '\0'))
+		{
+			count++;
+			i++;
+			j++;
+		}
+
+		if (count == LQ_strlen(needle))
+			return (&haystack[i - count]);
+		else
+			j = 0;
+		}
+	}
+	return (0);
+}

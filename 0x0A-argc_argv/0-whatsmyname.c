e4fdb860c9268c113e1c15e7038ea226e08ce3a0
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-06 19:18:15 -0800

    dded 0-whatsmyname.c, README.md, and main.h

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints its name
+ *
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+This README file contains a detailed description of the programs contained in this folder.
+
+  0-whatsmyname.c: This program prints its name, followed by a new line

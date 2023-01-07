7c16976a6970138f5f412bd4463bfd9b386d5c4b
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-07 09:27:37 -0800

    Corrected 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -54,24 +54,25 @@ int change(int value)
  * @argc: argument count
  * @argv: argument vector
  *
- * Return: 1 iferror, 0 otherwise
+ * Return: 1 if there's error, 0 otherwise
  */
 int main(int argc, char **argv)
 {
 	int cents;
 
-	cents = atoi(argv[1]);
+	cents = 0;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	
+	cents = atoi(argv[1]);
 	if (cents < 0)
 	{
 		printf("0\n");
-	} else
-	{
-		printf("%d\n", change(cents));
+		return (1);
 	}
+	printf("%d\n", change(cents));
 	return (0);
 }

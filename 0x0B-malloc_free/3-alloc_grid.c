38bae3f34027f756e5a83a801c97bb9d6bff6c21
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-09 17:28:14 -0800

    Corrected 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,6 +23,14 @@ int **alloc_grid(int width, int height)
 		for (i = 0; i < height; i++)
 		{
 			grid[i] = (int *) malloc(sizeof(int) * width);
+			if (grid[i] == NULL)
+			{
+				int tmp;
+
+				for (tmp = 0; tmp < i; tmp++)
+					free(grid[tmp]);	
+				return (NULL);
+			}
 		}
 		for (i = 0; i < height; i++)
 		{
@@ -31,7 +39,7 @@ int **alloc_grid(int width, int height)
 				grid[i][j] = 0;
 			}
 		}
+		return (grid);
 	}
-	return (grid);
-
+	return (NULL);
 }

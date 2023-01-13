804085bf25089654d65bc53820e13974bc97e841
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-12 17:05:57 -0800

    Corrected 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 	size = max - min + 1;
-	array = malloc(sizeof *array * size);
+	array = malloc(sizeof(*array) * size);
 	if (array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)

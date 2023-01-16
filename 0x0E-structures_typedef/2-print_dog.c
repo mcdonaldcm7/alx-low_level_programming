8c2058653ffaa406b41cb2902d82afc2fd6709d6
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-16 07:49:15 -0800

    Corrected 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,9 +11,9 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-	if (d->age < 0 || d->age > 20)
+	if (d->age >= 0)
 		printf("Age: %f\n", d->age);
 	else
 		printf("Age: (nil)");
-	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nill)");
+	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 }

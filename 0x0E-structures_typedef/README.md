f5bc4d22e4df93fce4f7292425ffc39dcad5d47a
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-16 03:24:47 -0800

    Added dog.h and README.md

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+This README fle contains a desctiption of the function(s) and struct contained in the dog.h header file
+
+  struct dog: A C structure with three member variables name, age, and owner.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - A C structure representing a dog
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

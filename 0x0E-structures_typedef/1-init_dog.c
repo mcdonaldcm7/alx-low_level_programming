b3b158a03a4062e0c7b7390a0aab4b381f7ac173
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-16 03:40:00 -0800

    Added 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ *
+ * @d: struct dog to initialize
+ * @name: dog's name to initialize d member name variable with
+ * @age: dog's age to initialize d member age variable with
+ * @owner: owner's name to initialize d memeber owner variable with
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,3 +1,5 @@
 This README fle contains a desctiption of the function(s) and struct contained in the dog.h header file
 
   struct dog: A C structure with three member variables name, age, and owner.
+
+  1-init_dog.c: A function that initialize a variable of type struct dog.

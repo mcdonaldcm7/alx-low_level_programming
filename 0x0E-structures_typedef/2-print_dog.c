820c04536007877cd0616966211e48cc5833738a
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-16 04:27:08 -0800

    Corrected 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,6 +11,9 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
-	printf("Name: %f\n", d->age);
+	if (d->age < 0 || d->age > 20)
+		printf("Age: %f\n", d->age);
+	else
+		printf("Age: (nil)");
 	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nill)");
 }

3bb2c209f1c5bb3a222005475ed690b3afda557f
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-16 16:32:41 -0800

    Added 5-free_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,5 @@ void init_dog(struct dog *, char *, float, char *);
 void print_dog(struct dog *);
 typedef struct dog dog_t;
 dog_t *new_dog(char *, float, char *);
+void free_dog(dog_t *);
 #endif

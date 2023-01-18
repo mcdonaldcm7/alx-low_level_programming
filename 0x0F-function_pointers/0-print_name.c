2e05f2ec190320e634bb0ce4eba64f99db3ec518
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-18 04:40:40 -0800

    Added 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name with the given function
+ *
+ * @name: Name to print
+ * @f: Function to print name
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+This file contains a detailed description of the files contained in this directory.
+
+	0-print_name.c: Prints a name with the given function

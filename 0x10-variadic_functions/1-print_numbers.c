1d0f6889c594289274fee90f30c2f7448294d849
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-19 15:52:56 -0800

    Corrected 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *seperator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (seperator == NULL)
+	if (seperator == NULL || n == 0)
 		return;
 	va_start(args, n);
 	for (i = 0; i < n; i++)

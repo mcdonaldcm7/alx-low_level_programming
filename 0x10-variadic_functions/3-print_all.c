daf04b0eaad1edec706625583fcf589d22d9bc10
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-19 15:43:21 -0800

    Added 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,53 +1,54 @@
 #include "variadic_functions.h"
 #include <stdio.h>
+#include <stdarg.h>
 
-void opr::print_int(int a)
-{
-	printf("%d", a);
-}
-
-void opr::print_str(char *s)
-{
-	printf("%s", s);
-}
-
-void opr::print_char(char c)
-{
-	printf("%c", c);
-}
-
-void opr::print_float(float f)
-{
-	printf("%f", f);
-}
-
-void print_all(const char * const format, ...)
+/**
+ * print_all - Prints anything
+ *
+ * @fmt: String containing the format of the parameters
+ * @...: Varags
+ *
+ * Return: Nothing
+ */
+void print_all(const char * const fmt, ...)
 {
 	int i;
 	va_list args;
-	opr_t ops[];
 
-	ops = {
-		{'c', print_char},
-		{'i', print_int},
-		{'f', print_float},
-		{'s', print_str}
-	};
-	va_start(args, format);
+	va_start(args, fmt);
 	i = 0;
-	while (format[i] != '\0')
+	while (fmt[i] != '\0')
 	{
-		char tmp1, tmp2;
+		char *s;
 
-		tmp1 = format[i];
-		tmp2 = va_arg(args, char);
-		if (tmp1 != 'c' && tmp1 != 'i' && tmp1 != 'f' && tmp1 != 's')
+		if (i && (fmt[i] == 'c' || fmt[i] == 'i' ||
+					fmt[i] == 'f' || fmt[i] == 's'))
+			printf(", ");
+		switch (fmt[i])
 		{
-			printf("(nil)");
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", s);
+				break;
+			default:
+				break;
 		}
-		if (tmp1 == ops[i].fmt)
-		{
-			ops[i].
-		}
+		i++;
 	}
+	va_end(args);
+	printf("\n");
 }

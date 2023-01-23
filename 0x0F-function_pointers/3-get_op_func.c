eee0938523ef66446bd3d091f79634bc756fcefd
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-23 02:31:23 -0800

    Corrected 3-main.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,7 +21,7 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (ops[i].op != NULL && ops[i].f != NULL)
+	while (ops[i].op != NULL || ops[i].f != NULL)
 	{
 		if (ops[i].op[0] == s[0])
 			return (ops[i].f);

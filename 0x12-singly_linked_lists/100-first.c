22e6e5495a5377368988bef9a4d6c49453a13cec
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-26 15:17:20 -0800

    Added 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * before_main - Prints a text before the main function
+ *
+ * Return: Nothing
+ */
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

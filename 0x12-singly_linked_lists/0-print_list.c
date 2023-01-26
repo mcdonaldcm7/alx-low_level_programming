2de814646ced0d127639892e88d166285a129bab
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-26 05:50:12 -0800

    Added 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_lists - Prints all the elements of a list_t list
+ *
+ * @h: Pointer to the head of the list
+ *
+ * Return: Number of nodes
+ */
+unsigned int print_list(const list_t *h)
+{
+	if (h != ((void *) 0))
+	{
+		if (h->str != ((void *) 0))
+			printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+	}
+	if (h->next == ((void *) 0))
+	{
+		return (1);
+	}
+	return (1 + print_list(h->next));
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+This file contains file with function definitions for the functions whoose declaration are included in the lists.h header file.
+
+	print_list: A function to print all the elements of a list_t (A linked list) list.

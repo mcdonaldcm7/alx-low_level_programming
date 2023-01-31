60491e4a846d4f66cc90b9cd49fdc48ee50522e1
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-31 01:50:26 -0800

    Included a descripption for the solution file

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index: Returns the nth node of a listint_t linked list.
+ * get_nodeint_at_index -  Returns the nth node of a listint_t linked list.
  *
  * @head: Pointer to the head of the node
  * @index: Index of the node to be retrieved

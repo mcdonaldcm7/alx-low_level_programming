389a6a194a8b50a09e6be3a23e28c3f9024e5bd9
LQ McDonald <mcdonald.cm7@gmail.com>
2023-01-31 11:29:24 -0800

    Corrected task 9 solution

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,6 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (idx == 0 && i == 0)
 		{
 			new->next = navg;
+			*head = new;
 			break;
 		}
 		if (idx > 0 && (i == (idx - 1)))

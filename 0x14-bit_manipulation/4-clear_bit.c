d3a2735941488aea0b282db4ef834edeff1c0881
LQ McDonald <mcdonald.cm7@gmail.com>
2023-02-02 13:48:31 -0800

    Solution file for task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * clear_bit - Clears the value of a bit to 1 at a given index
+ *
+ * @n: Integer value
+ * @index: Index, starting from 0 of the bit you want to clear
+ *
+ * Return: 1 if it worked, or -1 if it failed
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int clear;
+
+	if (n == (void *) 0)
+		return (-1);
+	clear = 1 << index;
+	if ((*n - set) > 0)
+	{
+		*n -= set;
+		return (1);
+	}
+	return (-1);
+}

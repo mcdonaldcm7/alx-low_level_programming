af3e96f627f840468f859c74b0c6f4f08139cb36
LQ McDonald <mcdonald.cm7@gmail.com>
2023-02-03 16:40:04 -0800

    Task 3 solution file

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -21,4 +21,21 @@ int set_bit(unsigned long int *n, unsigned int index)
 		return (1);
 	}
 	return (-1);
+/**
+ * set_bit - Sets the value of a bit to 1 at a given index
+ *
+ * @n: Integer value
+ * @index: Index, starting from 0 of the bit you want to set
+ *
+ * Return: 1 if it worked, or -1 if it failed
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+        unsigned long int set;
+
+        if (n == (void *) 0)
+                return (-1);
+        set = 1 << index;
+	*n += set;
+	return (1);
 }

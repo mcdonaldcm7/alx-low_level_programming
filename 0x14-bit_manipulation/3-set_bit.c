9e042f38a10d5297df0add42e33391dc487f0900
LQ McDonald <mcdonald.cm7@gmail.com>
2023-02-03 16:49:58 -0800

    Task 3 solution file

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -15,7 +15,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (n == (void *) 0)
 		return (-1);
 	set = 1 << index;
-	if (index > 0 && set <= 1)
+	if (index > 0 && (set <= 1 || set > *n))
 		return (-1);
 	*n += set;
 	return (1);

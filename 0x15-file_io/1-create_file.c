659858d5183ae99c3a5f290b524d404ac1243b52
LQ McDonald <mcdonald.cm7@gmail.com>
2023-02-06 16:29:45 -0800

    Solution file for task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * lq_strlen - Retrieves the length of the argument string
+ *
+ * @text: Argument string
+ *
+ * Return: Length of text
+ */
+int lq_strlen(char *text)
+{
+	if (text == (void *) 0 || text[0] == '\0')
+		return (0);
+	return (1 + lq_strlen(&text[1]));
+}
+
+/**
+ * create_file - Creates a file, writes the argument string to it if provided
+ * 
+ * @filename: Name to give the file to create
+ * @text_content: Argument string to write to the newly created file
+ *
+ * Return: 1 if successful and -1 if it failed.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, size, w;
+	
+	size = lq_strlen(text_content);
+	if (filename == (void *) 0)
+		return -1;
+	file = open(filename, (O_CREAT | O_RDWR | O_TRUNC));
+	if (file == -1)
+		return (-1);
+	if (size > 0)
+	{
+		w = write(file, text_content, size);
+	}
+	close(file);
+	return ((w >= 0 && w == size) ? 1 : -1);
+}

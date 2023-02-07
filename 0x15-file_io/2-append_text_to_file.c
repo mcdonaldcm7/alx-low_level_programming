7ba2b17706a4ffb17fdc5d5d582c4388e1d58dde
LQ McDonald <mcdonald.cm7@gmail.com>
2023-02-06 17:14:33 -0800

    Task 2 solution file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * lq_strlen - Retrieves the length of a string
+ *
+ * @s: Input string
+ *
+ * Return: Length of s
+ */
+int lq_strlen(char *s)
+{
+	if (s == (void *) 0 || s[0] == '\0')
+		return (0);
+	return (1 + lq_strlen(&s[1]));
+}
+
+/**
+ * append_text_to_file - Appends text at the end of a file
+ *
+ * @filename: The name of the file
+ * @text_content: Null terminated string to append to the end of the file
+ *
+ * Return: 1 on success and -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, w, s;
+
+	if (filename == (void *) 0)
+		return (-1);
+	s = lq_strlen(text_content);
+	fd = open(filename, O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (s > 0)
+	{
+		w = write(fd, text_content, s);
+		return ((w != -1 && w == s) ? 1 : -1);
+	}
+	
+}

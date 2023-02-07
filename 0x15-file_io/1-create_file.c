e6c344434c72dfface2fe43845f447ccd0c52ff6
LQ McDonald <mcdonald.cm7@gmail.com>
2023-02-06 16:45:14 -0800

    Solution file for task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,7 +32,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == (void *) 0)
 		return (-1);
 	file = open(filename,
-			(O_CREAT | O_RDWR | O_TRUNC), (S_IRUSR & S_IWUSR));
+			(O_CREAT | O_RDWR | O_TRUNC), (S_IRUSR | S_IWUSR));
 	if (file == -1)
 		return (-1);
 	if (size > 0)

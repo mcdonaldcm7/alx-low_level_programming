41c0b12d5733b691962bdc3c559c9f73822147e0
LQ McDonald <mcdonald.cm7@gmail.com>
2023-02-09 15:22:39 -0800

    Tas 0 Solution file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,16 +4,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int lq_textlen(int fd)
+/**
+ * lq_textlen - Attempts to retrieve the index for the end of the file
+ *
+ * @filename: Name of the file
+ *
+ * Return: Index for the end of the file
+ */
+unsigned int lq_textlen(const char *filename)
 {
-	int bytes, count;
+	int bytes, fd;
+	unsigned int count;
 	char c;
 
 	count = 0;
+	if ((fd = open(filename, O_RDONLY)) < 0)
+		return (-1);
 	while ((bytes = read(fd, &c, sizeof(c))) > 0)
 	{
 		count++;
 	}
+	close(fd);
 	return (count);
 }
 
@@ -43,15 +54,16 @@ unsigned int read_textfile(const char *filename, unsigned int letters)
 		return (0);
 	}
 
-	size = lq_textlen(fd);
-	count = read(fd, buf, ((size > letters) ? letters : size));
+	size = lq_textlen(filename);
+	size = size >= letters ? letters : size;
+	count = read(fd, buf, size);
 
 	if (count != -1)
 	{
-		w = write(STDOUT_FILENO, buf, count);
+		w = write(STDOUT_FILENO, buf, size);
 	}
 	close(fd);
 	free(buf);
-	return ((count >= 0 && w == letters) ? count : 0);
+	return ((count >= 0 && w == size) ? count : 0);
 }
 

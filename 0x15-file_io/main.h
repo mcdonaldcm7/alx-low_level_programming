3fe6cd18ddc1f4cca782cea586e3703b405631a6
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-01 23:35:37 +0100

    Updated main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,4 +3,8 @@
 unsigned int read_textfile(const char *, unsigned int);
 int create_file(const char *, char *);
 int append_text_to_file(const char *, char *);
+int _putchar(char);
+int lq_textlen(char *);
+int read_from(int, char *, int, char *);
+int write_to(int, char *, int, char *);
 #endif

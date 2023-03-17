84328f678b8f6f8826b8954a0922d1ee8ec0d5f3
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-17 23:19:26 +0100

    Task 7 (Fixed test case idx == 0)

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,37 +13,45 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int count;
-	dlistint_t *iter, *new;
+	dlistint_t *iter, *p_iter, *new;
 
 	count = 0;
 	iter = *h;
+	new = malloc(sizeof(*new));
+	if (new == (void *) 0)
+		return (new);
+	new->n = n;
+	if (*h == (void *) 0 || idx == 0)
+        {
+                new->prev = (void *) 0;
+		if (idx == 0)
+		{
+			new->next = *h;
+			(*h)->prev = new;
+		}
+                *h = new;
+                return (new);
+        }
 	while (iter != (void *) 0)
 	{
 		if (count == idx)
 		{
-			new = malloc(sizeof(*new));
-			if (new == (void *) 0)
-				return (new);
-			new->n = n;
 			new->prev = iter->prev;
 			new->prev->next = new;
 			iter->prev = new;
 			new->next = iter;
 			return (new);
 		}
+		p_iter = iter;
 		iter = iter->next;
 		count++;
 	}
-	if (*h == (void *) 0 && idx == 0)
+	if (count == idx)
 	{
-		new = malloc(sizeof(*new));
-		if (new == (void *) 0)
-			return (new);
-		new->n = n;
-		new->next = (void *) 0;
-		new->prev = (void *) 0;
-		*h = new;
+		new->prev = p_iter;
+		p_iter->next = new;
 		return (new);
 	}
+	free(new);
 	return ((void *) 0);
 }

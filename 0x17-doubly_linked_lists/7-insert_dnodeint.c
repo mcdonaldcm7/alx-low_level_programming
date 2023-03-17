e4d8c68598e8cfbb990213af2954a050972e7d61
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-03-17 23:29:44 +0100

    Task 7 (Fixed test case idx == 0)

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (*h == (void *) 0 || idx == 0)
         {
                 new->prev = (void *) 0;
-		if (idx == 0 && *h == (void *) 0)
+		if (idx == 0 && *h != (void *) 0)
 		{
 			new->next = *h;
 			(*h)->prev = new;

3ec3da6610df3e5ff74112b02b0ca51a9e95b201
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-04-10 02:30:46 +0100

    Definition for hash_table_get which is used to retrieve a value associated with a key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key
+ *
+ * @ht: The hash table you want to look into
+ * @key: The key you are looking for
+ *
+ * Return: Value associated with the element, or NULL if key couldn't be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t **at_index, *iter;
+
+	if (ht == (void *) 0 || key == (void *) 0 || _strlen(key) == 0)
+		return ((void *) 0);
+	index = key_index((unsigned char *) key, ht->size);
+	at_index = &ht->array[index];
+	if (*at_index == (void *) 0)
+		return ((void *) 0);
+	else
+	{
+		iter = *at_index;
+
+		while (iter)
+		{
+			if (_strcmp(iter->key, key) == 0)
+			{
+				return (iter->value);
+			}
+			iter = iter->next;
+		}
+	}
+	return ((void *) 0);
+}

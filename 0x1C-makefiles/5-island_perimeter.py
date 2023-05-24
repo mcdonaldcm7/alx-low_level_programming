826e01507248db20630612d628784129e3b11127
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-05-25 00:23:02 +0100

    Tweaked algorithm

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,9 +12,14 @@ def island_perimeter(grid):
     if grid is None or not isinstance(
             grid, list) or not isinstance(grid[0], list):
         return (0)
-    fence = list(0 for i in range(len(grid[0])))
+    fence_x = list(0 for i in range(len(grid[0])))
+    fence_y = list(0 for i in range(len(grid)))
     for i in range(len(grid)):
+        fence_y[y] += sum(grid[y])
         for j in range(len(grid[0])):
             if grid[i][j] > 0:
-                fence[j] += 1
+                fence_x[j] += 1
+    xtra = 0
+    if len(grid) != len(grid[0]):
+        return (4 * max(fence_x) + 2 * (max(fence_y) - 1))
     return (4 * max(fence))
